b3cc149941c55dd4cf584ba847cc1003515d6635
iffxxxx <iffxxxx@gmail.com>
2023-05-23 20:25:51 +0900

    Add Python File [0_Pizza(2).py]

---
+++ 0_Pizza(2).py
@@ -0,0 +1,21 @@
+# 머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다.
+# 피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때,
+# n명이 주문한 피자를 남기지 않고 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+
+def solution(n):
+    if n % 6 == 0:
+        answer = n//6
+    else:
+        for i in range(1, 100):
+            if 6*i % n == 0:
+                break
+        answer = i
+    return answer
+
+# def solution(n):
+#     answer = 1
+#     while 6 * answer % n:   # 1 == True
+#         answer += 1
+#     return answer
+
+print(solution(4))
