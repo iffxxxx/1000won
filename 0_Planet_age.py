8adb027af5c0942a0d6bbb5c0cc097ab74309fb6
iffxxxx <iffxxxx@gmail.com>
2023-05-25 21:47:19 +0900

    Add Python File [0_Planet_age.py]_2

--- 0_Planet_age.py
+++ 0_Planet_age.py
@@ -14,14 +14,14 @@ def solution(age):
 #     answer = ''
 #     alpha = {'0':'a', '1':'b', '2':'c', '3':'d', '4':'e', '5':'f',
 #             '6':'g', '7':'h', '8':'i', '9':'j'}
-    
 #     for i in str(age):
 #         answer += (alpha[i])
-#         answer = ''.join(alpha[i] for i in str(age))
+#     #'or'
+#     answer = ''.join(alpha[i] for i in str(age))
 #     return answer
 
 
 # def solution(age):
-#     return ''.join([chr(int(i)+97) for i in str(age)])
+#     return5 ''.join([chr(int(i)+97) for i in str(age)])
 
 print(solution(23))
