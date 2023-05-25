a98ec9c29ce7162bfa4a711fb2c9795a4dbfb756
iffxxxx <iffxxxx@gmail.com>
2023-05-25 21:44:16 +0900

    Add Python File [0_Planet_age.py]

---
+++ 0_Planet_age.py
@@ -0,0 +1,27 @@
+# 우주여행을 하던 머쓱이는 엔진 고장으로 PROGRAMMERS-962 행성에 불시착하게 됐습니다. 입국심사에서 나이를 말해야 하는데, PROGRAMMERS-962 행성에서는 나이를 알파벳으로 말하고 있습니다.
+# a는 0, b는 1, c는 2, ..., j는 9입니다.
+# 예를 들어 23살은 cd, 51살은 fb로 표현합니다. 나이 age가 매개변수로 주어질 때 PROGRAMMER-962식 나이를 return하도록 solution 함수를 완성해주세요.
+
+def solution(age):
+    answer = ''
+    Age = list(enumerate(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']))
+    for i in str(age):
+        answer += (Age[int(i)][1])
+    return answer
+
+
+# def solution(age):
+#     answer = ''
+#     alpha = {'0':'a', '1':'b', '2':'c', '3':'d', '4':'e', '5':'f',
+#             '6':'g', '7':'h', '8':'i', '9':'j'}
+    
+#     for i in str(age):
+#         answer += (alpha[i])
+#         answer = ''.join(alpha[i] for i in str(age))
+#     return answer
+
+
+# def solution(age):
+#     return ''.join([chr(int(i)+97) for i in str(age)])
+
+print(solution(23))
