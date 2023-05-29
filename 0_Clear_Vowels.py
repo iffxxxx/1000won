3402b0c385302cf60378c2dd329a6015139f88ad
iffxxxx <iffxxxx@gmail.com>
2023-05-29 23:04:27 +0900

    Add Python File [0_Clear_Vowels.py]

---
+++ 0_Clear_Vowels.py
@@ -0,0 +1,13 @@
+# 영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다.
+# 문자열 my_string이 매개변수로 주어질 때 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+def solution(my_string):
+    answer = my_string.translate({ord(letter) : None for letter in 'aeiou'})
+    return answer
+
+
+# def solution(my_string):
+#     return ''.join([i for i in my_string if not (i in 'aeiou')])
+
+
+print(solution('bus'))
