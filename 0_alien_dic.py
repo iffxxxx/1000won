e8b9a0c57bd7c26f8af16a0e651f2a0c2e1ec7c0
iffxxxx <iffxxxx@gmail.com>
2023-06-09 19:44:42 +0900

    Add Python File [0_alien_dic.py][set()]

---
+++ 0_alien_dic.py
@@ -0,0 +1,17 @@
+# PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다. 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다. spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
+
+def solution(spell, dic):
+    for i in dic:
+        if sorted((set(spell))) == sorted(set(i)):
+            return 1
+    return 2
+
+
+# def solution(spell, dic):
+#     spell = set(spell)
+#     for s in dic:
+#         if not spell-set(s):
+#             return 1
+#     return 2
+
+print(solution(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"]))
