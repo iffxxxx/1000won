3f2d37024678d02223bccb27ef3160ace1cd8d16
iffxxxx <iffxxxx@gmail.com>
2023-06-21 23:47:26 +0900

    Add Python File [0_Babbling.py][re.compile(), re.match()]

---
+++ 0_Babbling.py
@@ -0,0 +1,29 @@
+# 머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다.
+# 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다.
+# 문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+
+def solution(babbling):
+    answer = 0
+    form = ["aya", "ye", "woo", "ma"]
+    for j in babbling:
+        for i in form:
+            j = j.replace(i, "1")
+        if j.replace("1", '') == '':
+            answer += 1
+    return answer
+
+
+# Solution 2 --- re.complie, match
+# import re
+
+# def solution(babbling):
+#     regex = re.compile('^(aya|ye|woo|ma)+$')
+#     cnt=0
+#     for e in babbling:
+#         if regex.match(e):
+#             cnt+=1
+#     return cnt
+
+
+print(solution(["aya", "yee", "u", "maa", "wyeoo"]))
+
